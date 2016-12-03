b4dc7776f783923dcf0f4a8556c35fd996e786e2
Henry Richardson <hrichardson83@gmail.com>
2016-12-03 15:12:10 -0500

    first commit

---
+++ .tmp/concat/scripts/scripts.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name amcTopSongApp
+ * @description
+ * # amcTopSongApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('amcTopSongApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name amcTopSongApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the amcTopSongApp
+ */
+angular.module('amcTopSongApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+angular.module('amcTopSongApp').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('views/main.html',
+    "<div class=\"jumbotron\"> <h1>'Allo, 'Allo!</h1> <p class=\"lead\"> <img src=\"images/yeoman.png\" alt=\"I'm Yeoman\"><br> Always a pleasure scaffolding your apps. </p> <p><a class=\"btn btn-lg btn-success\" ng-href=\"#/\">Splendid!<span class=\"glyphicon glyphicon-ok\"></span></a></p> </div> <div class=\"row marketing\"> <h4>HTML5 Boilerplate</h4> <p> HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites. </p> <h4>Angular</h4> <p> AngularJS is a toolset for building the framework most suited to your application development. </p> <h4>Karma</h4> <p>Spectacular Test Runner for JavaScript.</p> </div>"
+  );
+
+}]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name amcTopSongApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the amcTopSongApp
+ */
+angular.module('amcTopSongApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
