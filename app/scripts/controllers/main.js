880a84b28a29a06e72eb1d9c1b48866057b4d815
Henry Richardson <hrichardson83@gmail.com>
2016-12-08 11:42:31 -0500

    added other search results and JSON as Object for expert.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,9 +11,13 @@ angular.module('amcTopSongApp')
   .controller('MainCtrl', function ($scope,Spotify) {
     $scope.playlistTitle = 'My Top 10 Songs';
     $scope.playlist = [];
+    $scope.playlistObj = {
+      title: $scope.playlistTitle,
+      songs: $scope.playlist
+    }
     $scope.addToPlaylist = function() {
       $scope.playlist.push($scope.inPlaylist);
-      console.log($scope.playlist);
+      console.log($scope.playlistObj);
     }
     $scope.removeFromPlaylist = function(track) {
       $scope.playlist.splice($scope.playlist.indexOf(track),1);
@@ -22,22 +26,68 @@ angular.module('amcTopSongApp')
       Spotify.search($scope.searchArtistInput, 'artist').then(function (data) {
         $scope.searchArtistVal = data;
         // console.log(data);
-        $scope.showArtists = true;  
+        $scope.showArtists = true; 
+        $scope.showAlbums = false; 
+        $scope.showTracks = false; 
+
+      });
+    };
+    $scope.searchAlbumFunc = function(id) {
+      Spotify.search($scope.searchAlbumInput, 'album').then(function (data) {
+        $scope.searchAlbumVal = data;
+        // console.log(data);
+        
+        $scope.showArtists = false;
+        $scope.showAlbums = true;  
+        $scope.showTracks = false; 
+      });
+    };
+    $scope.searchSongFunc = function(id) {
+      
+      Spotify.search($scope.searchSongInput, 'track').then(function (data) {
+        $scope.searchTrackVal = data;
+        // console.log(data);
+        $scope.showArtists = false;
+        $scope.showAlbums = false;  
         $scope.showTracks = true; 
       });
+    };
+    $scope.clearAll = function(id) {
+        $scope.searchArtistInput = null;
+        $scope.searchAlbumInput = null;
+        $scope.searchSongInput = null;
+        // console.log("clear all");
+        $scope.showArtists = false;
+        $scope.showAlbums = false;  
+        $scope.showTracks = false; 
+
     };
     $scope.selectArtist = function(id) {
       Spotify.getArtistTopTracks(id, 'US')
       .then(function (data) {
         $scope.selectedArtist = data;
           // console.log(data);
-        $scope.showArtists = true;  
+        $scope.showArtists = true;
+        $scope.showAlbums = false;  
         $scope.showTracks = true;  
       });
     };
+    $scope.selectAlbum = function(id) {
+      Spotify.getAlbum(id)
+      .then(function (data) {
+        $scope.selectedAlbumVal = data;
+          // console.log("this one");
+        $scope.showArtists = false;  
+        $scope.showAlbums = true;  
+        $scope.showTracks = true;  
+      });
+    };
+    
+    
     $scope.AddTracktoPlaylist = function(id) {
       Spotify.getTrack(id).then(function (data) {
         $scope.selectedTrack = data;
+        // create array to input
         $scope.inPlaylist = {
           track: $scope.selectedTrack.name,
           artist: $scope.selectedTrack.artists[0].name,
@@ -46,10 +96,9 @@ angular.module('amcTopSongApp')
           customImage: 'https://www.stitch.net/wp-content/uploads/2014/10/9.jpg' ,
           id: $scope.selectedTrack.id
         }
+        // Add the item to the song array
         $scope.addToPlaylist();
-          // console.log($scope.inPlaylist);
-        $scope.showArtists = true;  
-        $scope.showTracks = true; 
+
       });
     };
   });
